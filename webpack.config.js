f6cd063aa47b83d2711fb765a3aa88dafe39674d
Peng Ai <aaaipeng@gmail.com>
2021-06-02 09:09:48 -0700

    updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     path: DIST_DIR,
-    filename: 'bundle.js',
-    chunkFilename: '[name].bundle.js'
+    filename: 'bundle.js'
   },
   module: {
     rules: [
